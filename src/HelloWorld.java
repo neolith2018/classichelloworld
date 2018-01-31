5abf7a512d7c3c110e30a05ba825cb3d975cc6a5
Chang <Chang@Chang-main>
2018-01-31 16:11:01 +0800

    Add the multiplication table.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,11 @@ public class HelloWorld {
 	public static void main(String[] argv) {
 		
 		System.out.println("Hello world!");
+		
+		for (int i = 1; i < 10; i++) {
+			for (int j = 1; j < 10; j++) {
+				System.out.println(i + " * " + j + " = " + (i * j));
+			}
+		}
 	}
 }
