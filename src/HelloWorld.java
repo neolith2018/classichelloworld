c1a8389458eee0ac3e5dfdbcdb065677c5d8beeb
Chang <Chang@Chang-main>
2018-01-31 16:39:27 +0800

    Change from addition to deletion.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 		
 		for (int i = 1; i < 10; i++) {
 			for (int j = 1; j < 10; j++) {
-				System.out.println(i + " + " + j + " = " + (i + j));
+				System.out.println(i + " - " + j + " = " + (i - j));
 			}
 		}
 	}
